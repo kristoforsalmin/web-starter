0cd7c8e81cd0358c4a877f29c4a671e5bb139646
Kristofor Salmin <racse1@users.noreply.github.com>
2016-07-27 11:18:59 +0300

    Update ESLint config and gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,27 @@
-const autoprefixer = require('autoprefixer');
-const bourbon = require('bourbon');
-const browserSync = require('browser-sync');
-const del = require('del');
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
-const nunjucks = require('gulp-nunjucks');
-const postcss = require('gulp-postcss');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const webpack = require('webpack');
+/* eslint-disable import/no-extraneous-dependencies */
+import autoprefixer from 'autoprefixer';
+import bourbon from 'bourbon';
+import browserSync from 'browser-sync';
+import del from 'del';
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import htmlmin from 'gulp-htmlmin';
+import imagemin from 'gulp-imagemin';
+import nunjucks from 'gulp-nunjucks';
+import postcss from 'gulp-postcss';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import webpack from 'webpack';
 
 function isProduction() {
-  return gutil.env.env === 'production';
+  return process.env.NODE_ENV || 'development';
 }
 
 function streamError(error) {
-  gutil.beep();
   gutil.log(gutil.colors.red(error.toString()));
+  gutil.beep();
 
-  if (typeof this.emit === 'function') {
+  if (this && typeof this.emit === 'function') {
     this.emit('end');
   }
 }
@@ -36,93 +37,89 @@ function compileError(error, stats) {
 }
 
 /**
- * Paths
+ * Config
  */
 
 const PATH_SRC = './src';
 const PATH_DEST = './dist';
 const PATH_MODULES = './node_modules';
 
-/**
- * Config
- */
-
 const config = {
   serve: {
     open: false,
     notify: false,
     server: {
-      baseDir: PATH_DEST
-    }
+      baseDir: PATH_DEST,
+    },
   },
 
   javascripts: {
     entry: {
-      main: `${PATH_SRC}/javascripts/main.js`
+      main: `${PATH_SRC}/javascripts/main.js`,
     },
     output: {
       path: `${PATH_DEST}/javascripts`,
-      filename: '[name].js'
+      filename: '[name].js',
     },
     module: {
       loaders: [
-        { test: /\.js$/, loader: ['babel-loader', 'eslint-loader'], exclude: PATH_MODULES }
-      ]
+        { test: /\.js$/, loader: ['babel-loader', 'eslint-loader'], exclude: PATH_MODULES },
+      ],
     },
     plugins: [],
-    devtool: 'source-map'
+    devtool: 'source-map',
   },
 
   stylesheets: {
     paths: {
       src: `${PATH_SRC}/stylesheets/**`,
-      dest: `${PATH_DEST}/stylesheets`
+      dest: `${PATH_DEST}/stylesheets`,
     },
     sass: {
       outputStyle: isProduction() ? 'compressed' : 'expanded',
-      includePaths: [bourbon.includePaths]
+      includePaths: [bourbon.includePaths],
     },
     autoprefixer: {
-      browsers: ['last 2 versions']
-    }
+      browsers: ['last 2 versions'],
+    },
   },
 
   images: {
     paths: {
       src: `${PATH_SRC}/images/**`,
-      dest: `${PATH_DEST}/images`
+      dest: `${PATH_DEST}/images`,
     },
     imagemin: {
-      progressive: true
-    }
+      progressive: true,
+    },
   },
 
   fonts: {
     paths: {
       src: `${PATH_SRC}/fonts/**`,
-      dest: `${PATH_DEST}/fonts`
-    }
+      dest: `${PATH_DEST}/fonts`,
+    },
   },
 
   html: {
     paths: {
       src: `${PATH_SRC}/**/*.html`,
-      dest: PATH_DEST
+      dest: PATH_DEST,
     },
     htmlmin: {
-      collapseWhitespace: isProduction()
-    }
+      collapseWhitespace: isProduction(),
+    },
   },
 
   copy: {
     paths: {
       src: [
         `${PATH_SRC}/*`,
-        `!${PATH_SRC}/*.html`
+        `!${PATH_SRC}/*.html`,
       ],
-      dest: PATH_DEST
-    }
-  }
+      dest: PATH_DEST,
+    },
+  },
 };
 
 if (isProduction()) {
@@ -149,40 +146,40 @@ gulp.task('javascripts', () => {
   });
 });
 
-gulp.task('stylesheets', () => {
-  return gulp.src(config.stylesheets.paths.src)
+gulp.task('stylesheets', () => (
+  gulp.src(config.stylesheets.paths.src)
     .pipe(sourcemaps.init())
     .pipe(sass(config.stylesheets.sass))
     .on('error', streamError)
     .pipe(postcss([autoprefixer(config.stylesheets.autoprefixer)]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.stylesheets.paths.dest))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+));
 
-gulp.task('images', () => {
-  return gulp.src(config.images.paths.src)
+gulp.task('images', () => (
+  gulp.src(config.images.paths.src)
     .pipe(imagemin(config.images.imagemin))
-    .pipe(gulp.dest(config.images.paths.dest));
-});
+    .pipe(gulp.dest(config.images.paths.dest))
+));
 
-gulp.task('fonts', () => {
-  return gulp.src(config.fonts.paths.src)
-    .pipe(gulp.dest(config.fonts.paths.dest));
-});
+gulp.task('fonts', () => (
+  gulp.src(config.fonts.paths.src)
+    .pipe(gulp.dest(config.fonts.paths.dest))
+));
 
-gulp.task('html', () => {
-  return gulp.src(config.html.paths.src)
+gulp.task('html', () => (
+  gulp.src(config.html.paths.src)
     .pipe(nunjucks.compile())
     .on('error', streamError)
     .pipe(htmlmin(config.html.htmlmin))
-    .pipe(gulp.dest(config.html.paths.dest));
-});
+    .pipe(gulp.dest(config.html.paths.dest))
+));
 
-gulp.task('copy', () => {
-  return gulp.src(config.copy.paths.src, { dot: true })
-    .pipe(gulp.dest(config.copy.paths.dest));
-});
+gulp.task('copy', () => (
+  gulp.src(config.copy.paths.src, { dot: true })
+    .pipe(gulp.dest(config.copy.paths.dest))
+));
 
 gulp.task('clean', () => {
   del([`${PATH_DEST}/*`]);
