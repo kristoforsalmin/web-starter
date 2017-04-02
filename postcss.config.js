59218d87433913d78876be1a8a8f58cc9c201a5b
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2017-04-02 22:44:56 +0300

    Update dependencies and postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  log: true,
   map: true,
-  use: [
-    'postcss-import',
-    'postcss-custom-media',
-    'postcss-custom-properties',
-    'autoprefixer',
-    'postcss-csso',
-  ],
+  plugins: {
+    'postcss-import': {},
+    'postcss-custom-media': {},
+    'postcss-custom-properties': {},
+    'autoprefixer': {},
+    'postcss-csso': {},
+  },
 };
