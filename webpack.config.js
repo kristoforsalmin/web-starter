3f368138194e6247110fbe3a96a03a9d71fdf7f9
Kristofor Salmin <racse1@users.noreply.github.com>
2017-12-12 16:58:26 +0300

    Use faster source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  devtool: debug ? 'inline-source-map' : false,
+  devtool: debug ? 'cheap-module-eval-source-map' : false,
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
