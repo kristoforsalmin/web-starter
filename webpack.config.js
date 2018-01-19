5fc0731c8b95039548980b4c27eae7b63a204705
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2018-01-19 22:51:32 +0300

    Cache Babel transformations to the filesystem

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,30 @@ const webpack = require('webpack');
 
 const debug = process.env.NODE_ENV !== 'production';
 
+const srcDir = 'src';
+const destDir = 'dist';
+const scriptsDir = 'scripts';
+
 module.exports = {
-  context: path.resolve(__dirname, 'src/scripts'),
+  context: path.resolve(__dirname, `${srcDir}/${scriptsDir}`),
   entry: './main.js',
   output: {
-    path: path.resolve(__dirname, 'dist/scripts'),
-    publicPath: '/scripts/',
+    path: path.resolve(__dirname, `${destDir}/${scriptsDir}`),
+    publicPath: `/${scriptsDir}/`,
     filename: '[name].bundle.js',
     chunkFilename: '[name].[chunkhash].chunk.js',
   },
   devtool: debug ? 'cheap-module-eval-source-map' : false,
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, `${srcDir}/${scriptsDir}`),
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: debug,
+        },
+      },
     ],
   },
   plugins: [
